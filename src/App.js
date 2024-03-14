8b4298b7bdf6b338f87aac601c05d75cbbffacbb
riddhi <test01.rentechdigital@gmail.com>
2024-03-14 15:47:12 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           hello, good morning
-//         </p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
@@ -26,18 +8,15 @@ function App() {
     const [image, setImage] = useState()
     const [teams, setTeams] = useState([])
 
-console.log('teams==========', teams)
     const handleUpload = (e) => {
         const formdata = new FormData()
         formdata.append('file', file)
         axios.post('https://node-deploy-vercel-seven.vercel.app/upload', formdata)
-        // axios.post(`${window.location.origin}/upload`, formdata)
         .then(res => setImage(res.data.image))
         .catch(err => console.log(err))
     }
 
     const handleGet = (e) => {
-        // axios.get(`${window.location.origin}/getallTeam`)
         axios.get('https://node-deploy-vercel-seven.vercel.app/getallTeam')
         .then(res => {
             console.log(res.data, "========");
@@ -53,7 +32,6 @@ console.log('teams==========', teams)
                 <button onClick={handleUpload}>Upload</button>
                 <br/><br/><br/>
                 <img src={`https://node-deploy-vercel-seven.vercel.app/Images/` + image}  alt='' />
-                {/* <img src={`${window.location.origin}/Images/` + image}  alt='' /> */}
             </div>
             <br/><br/><br/><br/>
             <div>
